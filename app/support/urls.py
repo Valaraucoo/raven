94f543858e47e809973fcae9394e38fdbfc1dc68
Kamil <jestem.kamil.wozniak@gmail.com>
2020-12-10 17:56:47 +0100

    [#30] feat: support

---
+++ app/support/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from support import views
+
+app_name = 'support'
+
+urlpatterns = [
+    path('contact/', views.TicketCreateView.as_view(), name='support-contact'),
+]
